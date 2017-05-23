a839816c82a640b06a8e4a627c7a1a1c2198832f
Alexie McDonald <alexiemcdonald304@gmail.com>
2017-05-23 09:51:01 -0400

    Return the value of the libraries property and log the props

--- tech_stack/src/Components/common/LibraryList.js
+++ tech_stack/src/Components/common/LibraryList.js
@@ -3,12 +3,13 @@ import { connect } from 'react-redux';
 
 class LibraryList extends Component {
     render() {
+        console.log(this.props);
         return;
     }
 }
 
 const mapStateToProps = state => {
-    console.log(state);
+    return { libraries: state.libraries };
 };
 
 export default connect(mapStateToProps)(LibraryList);
