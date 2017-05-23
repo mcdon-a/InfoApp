dd0d0ffa880d8fa7053a28dfe42fd6c07d0d228e
Alexie McDonald <alexiemcdonald304@gmail.com>
2017-05-23 11:39:52 -0400

    Imported and called connect and actions

--- tech_stack/src/Components/ListItem.js
+++ tech_stack/src/Components/ListItem.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import { Text } from 'react-native';
+import { connect } from 'react-redux';
 import { CardSection } from './common';
+import * as actions from '../actions';
+//In order to go up from one library you add a dot
+//The import * as variable is for importing everything from a file and then assigning it
 
 class ListItem extends Component {
     render() {
         const { titleStyle } = styles;
+        console.log(this.props);
         return (
         <CardSection>
             <Text style={titleStyle}>{this.props.library.title}</Text>
@@ -19,4 +24,4 @@ const styles = {
         marginLeft: 15
     }
 };
-export default ListItem;
+export default connect(null, actions)(ListItem);
