211d330fa970fbcbed23f6dd8628365477939c29
Alexie McDonald <alexiemcdonald304@gmail.com>
2017-05-23 09:44:56 -0400

    Fixed imported LibraryList and fixed file location error

--- tech_stack/src/app.js
+++ tech_stack/src/app.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
 import Header from './Components/common/Header';
+import LibraryList from './Components/common/LibraryList';
 
 
 const App = () => {
@@ -12,6 +13,7 @@ const App = () => {
         <Provider store={createStore(reducers)}>
             <View>
             <Header headerText="Tech Stack" />
+            <LibraryList />
             </View>
         </Provider>
     );
