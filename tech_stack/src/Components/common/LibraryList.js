fc3802dceb2ae564f4b10eaec4b9c3fd7edb2c05
Alexie McDonald <alexiemcdonald304@gmail.com>
2017-05-23 10:20:23 -0400

    Import ListView and create componentWillMount and renderRow

--- tech_stack/src/Components/common/LibraryList.js
+++ tech_stack/src/Components/common/LibraryList.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
+import { ListView } from 'react-native';
 import { connect } from 'react-redux';
 
 class LibraryList extends Component {
+    componentWillMount() {
+        //The following is boilerplate. Copy it from code to code
+       const ds = new ListView.DataSource({
+           rowHasChanged: (r1, r2) => r1 !== r2
+       });
+       this.dataSource = ds.cloneWithRows(this.props.libraries);
+    }
+
+    renderRow() {
+
+    }
+    
     render() {
         console.log(this.props);
-        return;
+        return (
+            <ListView dataSource={this.dataSource} renderRow={this.renderRow} />
+        );
     }
 }
 
-const mapStateToProps = state => {
-    return { libraries: state.libraries };
-};
+const mapStateToProps = state => ({ libraries: state.libraries });
 
 export default connect(mapStateToProps)(LibraryList);
