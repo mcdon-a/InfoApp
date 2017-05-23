26a342df418d5cfd870a2790d782c379df6f303e
Alexie McDonald <alexiemcdonald304@gmail.com>
2017-05-23 08:45:47 -0400

    Imported Header

--- tech_stack/src/app.js
+++ tech_stack/src/app.js
@@ -3,12 +3,16 @@ import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
+import Header from './Components/common/Header';
 
 
 const App = () => {
     return (
+        //The provider can only have one child component
         <Provider store={createStore(reducers)}>
-            <View />
+            <View>
+            <Header headerText="Tech Stack" />
+            </View>
         </Provider>
     );
 };
