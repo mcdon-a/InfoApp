5443ca8ac15fbb98d6db04509e4837895dd11648
Alexie McDonald <alexiemcdonald304@gmail.com>
2017-05-23 11:04:11 -0400

    Added styling to the text tag

--- tech_stack/src/Components/ListItem.js
+++ tech_stack/src/Components/ListItem.js
@@ -4,12 +4,19 @@ import { CardSection } from './common';
 
 class ListItem extends Component {
     render() {
+        const { titleSyle } = styles;
         return (
         <CardSection>
-            <Text>{this.props.library.title}</Text>
+            <Text style={titleSyle}>{this.props.library.title}</Text>
         </CardSection>
         );
     }
 }
 
+const styles = {
+    titleStyle: {
+        fontSize: 18,
+        marginLeft: 15
+    }
+};
 export default ListItem;
