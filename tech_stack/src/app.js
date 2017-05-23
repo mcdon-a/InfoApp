2a91066f50dcd8eab0a29ac4cc967ef777904866
Alexie McDonald <alexiemcdonald304@gmail.com>
2017-05-23 10:45:56 -0400

    Redirected the file location

--- tech_stack/src/app.js
+++ tech_stack/src/app.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
 import Header from './Components/common/Header';
-import LibraryList from './Components/common/LibraryList';
+import LibraryList from './Components/LibraryList';
 
 
 const App = () => {
